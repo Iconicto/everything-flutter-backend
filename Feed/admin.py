bc465b2ce4dc988fe5ba8385cc901f78091537fd
Isala Piyarisi <code@isala.me>
2019-06-30 20:26:41 +0530

    Added Data Models

--- Feed/admin.py
+++ Feed/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'logo')
+    list_display_links = list_display
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'venue', 'date', 'description', 'registration_link')
+    list_display_links = list_display
+
+
+class TutorialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'published_date', 'link', 'source')
+    list_display_links = list_display
+
+
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'irc_link', 'community_link')
+    list_display_links = list_display
+
+
+class WidgetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image', 'link')
+    list_display_links = list_display
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'time', 'link', 'source', 'image')
+    list_display_links = list_display
+
+
+admin.site.register(Source, SourceAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Tutorial, TutorialAdmin)
+admin.site.register(Community, CommunityAdmin)
+admin.site.register(News, NewsAdmin)
+admin.site.register(Widget, WidgetAdmin)
--- Feed/models.py
+++ Feed/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
+
 # Create your models here.
+class Source(models.Model):
+    name = models.CharField(max_length=150)
+    logo = models.ImageField(upload_to='Source/logo/')
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=350)
+    time = models.DateTimeField(auto_now=True)
+    link = models.URLField()
+    source = models.ForeignKey(Source, on_delete=models.DO_NOTHING, related_name='NewsSource')
+    image = models.ImageField(upload_to='News/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=150)
+    venue = models.CharField(max_length=350)
+    organizer = models.CharField(max_length=350)
+    date = models.DateField()
+    time = models.TimeField()
+    description = models.TextField(blank=True, null=True)
+    registration_link = models.URLField()
+    banner = models.ImageField(upload_to='Event/Banner')
+
+    def __str__(self):
+        return self.name
+
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+    link = models.URLField()
+    source = models.ForeignKey(Source, on_delete=models.DO_NOTHING, related_name='TutorialSource')
+
+    def __str__(self):
+        return self.title
+
+
+class Community(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    irc_link = models.URLField(blank=True, null=True)
+    community_link = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Widget(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='Widget/', blank=True, null=True)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+
