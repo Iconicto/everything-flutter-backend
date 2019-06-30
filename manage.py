c3548fd4ec39ecc89cfb8e51ea153f6fd9680860
Isala Piyarisi <code@isala.me>
2019-06-30 22:04:09 +0530

    Create EverythingFlutter/static

--- manage.py
+++ manage.py
@@ -6,6 +6,8 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EverythingFlutter.settings')
+    if not os.path.isdir('EverythingFlutter/static'):
+        os.mkdir('EverythingFlutter/static')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
