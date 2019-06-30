b49d267b7e800c1c8f2ef877dce36fa04f30b433
Isala Piyarisi <code@isala.me>
2019-06-30 21:53:22 +0530

    Update Production Dockerfile

--- Deployment/Production/docker-entrypoint.sh
+++ Deployment/Production/docker-entrypoint.sh
@@ -7,3 +7,7 @@ python manage.py collectstatic --noinput
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate --noinput
+
+# Start server
+echo "Starting gunicorn server"
+gunicorn EverythingFlutter.wsgi -b 0.0.0.0:8000
